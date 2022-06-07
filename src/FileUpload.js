b63f031d286fa3bd41bf09cefaedaf79be70f59e
Luisa Sarmiento <luisas@keoworld.com>
2022-06-07 11:36:05 -0500

    table

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -3,20 +3,22 @@ import { ANDROID_192 } from '@keoworld/gbl-ui-kit/assets/logo'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { useAuth } from './auth'
 import Document from './Document'
 import storage, { db } from './firebase'
 
 const FileUpload = () => {
-  const [documents , setDocuments] = useState(null);
+  const [documents, setDocuments] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
   const [docsUrls, setDocsUrls] = useState({});
+  const { signOut } = useAuth()
   const options = [
     { value: 'ISR', label: 'ISR' }
   ]
 
   useEffect(() => {
     db.collection(`ocr`).onSnapshot((snapshot) => {
-      const dataDocs = snapshot.docChanges().reduce( (acc, itm) => {
+      const dataDocs = snapshot.docChanges().reduce((acc, itm) => {
         const doc = itm.doc.data()
         return {
           ...acc,
@@ -25,24 +27,24 @@ const FileUpload = () => {
             [doc?.type]: doc?.label
           }
         }
-      }, {}) 
-      setDocsUrls( prev => ({ ...prev, ...dataDocs }) )
+      }, {})
+      setDocsUrls(prev => ({ ...prev, ...dataDocs }))
     })
-/*
-      snapshot.docChanges().forEach((change) => {
-        console.log('data', change.doc.data())
-        if (change.type === "added") {
-          if (docsUrls[change.doc.data().value]){
-            var tempdoc = docsUrls[change.doc.data().value] 
-            tempdoc[change.doc.data().type] = change.doc.data().label
-            setDocsUrls((prev) => ({...prev, [change.doc.data().value]: tempdoc}))
-          } else {            
-            setDocsUrls((prev) => ({...prev, [change.doc.data().value]:{[change.doc.data().type]: change.doc.data().label}}), console.log(docsUrls))
-          }
-        }
-      });
-    }); */
-  },[]);    
+    /*
+          snapshot.docChanges().forEach((change) => {
+            console.log('data', change.doc.data())
+            if (change.type === "added") {
+              if (docsUrls[change.doc.data().value]){
+                var tempdoc = docsUrls[change.doc.data().value] 
+                tempdoc[change.doc.data().type] = change.doc.data().label
+                setDocsUrls((prev) => ({...prev, [change.doc.data().value]: tempdoc}))
+              } else {            
+                setDocsUrls((prev) => ({...prev, [change.doc.data().value]:{[change.doc.data().type]: change.doc.data().label}}), console.log(docsUrls))
+              }
+            }
+          });
+        }); */
+  }, []);
 
   const onHandleChange = (docs) => {
     setDocuments(docs[0]);
@@ -56,21 +58,21 @@ const FileUpload = () => {
     setDocuments(null);
   }
 
-  const upload = async ()=>{
-    if (documents == null  || selectedOption == null || selectedOption === 'base') {
-      console.log('NO pon algo')  
+  const upload = async () => {
+    if (documents == null || selectedOption == null || selectedOption === 'base') {
+      console.log('NO pon algo')
     } else {
       await storage.ref(`/docs/${documents.name}`).put(documents).then(async (snapshoot) => {
-          const url = await snapshoot.ref.getDownloadURL();
-          const doc = documents.name.split(".")
-          db.collection('ocr').doc().set({value:doc[0], label: url, type: doc[1]});
-        });    
+        const url = await snapshoot.ref.getDownloadURL();
+        const doc = documents.name.split(".")
+        db.collection('ocr').doc().set({ value: doc[0], label: url, type: doc[1] });
+      });
       back();
     }
   }
 
   const back = async () => {
-    await axios.post("https://ocrgunicorn-dot-gc-k-gbl-lab.uc.r.appspot.com/ocr",{"url": `docs/`+documents.name,"doctype": selectedOption})
+    await axios.post("https://ocrgunicorn-dot-gc-k-gbl-lab.uc.r.appspot.com/ocr", { "url": `docs/` + documents.name, "doctype": selectedOption })
   }
 
   const onHandleSubmit = (event) => {
@@ -83,34 +85,49 @@ const FileUpload = () => {
       <img alt='logo' src={ANDROID_192} />
       <h2>Lectura de Archivos</h2>
       <form className='file_upload'>
-        <Select 
+        <Select
           label='tipo de archivo'
           onChange={onHandleChangeSelect}>
-            <option value={'base'}>Seleccione tipo de archivo</option>
-                {options.map((e, key) => {
-                  return (
-                    <option key={key} value={e.value}>
-                      {e.label}
-                    </option>
-                  );
-            })}
+          <option value={'base'}>Seleccione tipo de archivo</option>
+          {options.map((e, key) => {
+            return (
+              <option key={key} value={e.value}>
+                {e.label}
+              </option>
+            );
+          })}
         </Select>
-        <p/>
-        <Document allowUpload allowedDocumentTypes=".pdf" documentName="file" key={documents} 
-        file={documents} label={"suba su documento"} onChange={onHandleChange} onClickDelete={deleteFile}/>
-        <p/>
+        <p />
+        <Document allowUpload allowedDocumentTypes=".pdf" documentName="file" key={documents}
+          file={documents} label={"suba su documento"} onChange={onHandleChange} onClickDelete={deleteFile} />
+        <p />
         <Button size='large' device='mobileLight' onClick={onHandleSubmit}>
           Cargar
-        </Button> 
+        </Button>
       </form>
-      <p/>
-      { Object.keys(docsUrls).map( (item, index) => {
-        return <div key={index}>
-          <a key={item+"pdf"} href={docsUrls[item]["pdf"]}> {item + ".pdf"} </a>
-          <a key={item+"xlsx"} href={docsUrls[item]["xlsx"]}> {item + ".excel"} </a>
-          </div>;
-      })}     
-      </SignInStyled>
+      <p />
+      <table className="fl-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>PDF</th>
+            <th>Excel</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(docsUrls).map((item, index) => {
+            return <tr key={index}>
+              <td key={item}>{item}</td>
+              <td><a key={item + "pdf"} href={docsUrls[item]["pdf"]}> pdf </a></td>
+              <td><a key={item + "xlsx"} href={docsUrls[item]["xlsx"]}> excel </a></td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
+      <Button className="sign-out" onClick={signOut}>
+        Log out
+      </Button>
+    </SignInStyled>
   )
 }
 
@@ -141,5 +158,82 @@ const SignInStyled = styled.div`
   button.new-account {
     margin: 0;
   }
+  .sign-out{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 10%
+  }
+  /* Table Styles */
+
+  .fl-table {
+    box-shadow: 0px 35px 50px rgba( 0, 0, 0, 0.2 );
+    border-radius: 5px;
+    border-collapse: collapse;
+    width: 70%;
+    max-width: 100%;
+}
+
+.fl-table td, .fl-table th {
+    text-align: center;
+    padding: 8px;
+}
+
+.fl-table td {
+    border-right: 1px solid #f8f8f8;
+    font-size: 12px;
+}
+
+.fl-table thead th {
+    color: #ffffff;
+    background: #FF4759;
+}
+
+/* Responsive */
+@media (max-width: 767px) {
+    .fl-table {
+        display: block;
+        width: 100%;
+    }
+    .table-wrapper:before{
+        content: "Scroll horizontally >";
+        display: block;
+        text-align: right;
+        font-size: 11px;
+        color: white;
+        padding: 0 0 10px;
+    }
+    .fl-table thead, .fl-table tbody, .fl-table thead th {
+        display: block;
+    }
+    .fl-table thead th:last-child{
+        border-bottom: none;
+    }
+    .fl-table thead {
+        float: left;
+    }
+    .fl-table tbody {
+        width: auto;
+        position: relative;
+        overflow-x: auto;
+    }
+    .fl-table td, .fl-table th {
+        padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 200px;
+    }
+    .fl-table tbody tr {
+        display: table-cell;
+    }
+   
+    .fl-table tbody td {
+        display: block;
+        text-align: center;
+    }
+}
 `
 export default FileUpload
